e3def8c7efcd646c3e47d1dc75a72e927945de9c
zhanfang <277208607@qq.com>
2016-03-29 19:42:18 +0800

    完成handler text

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 var koa = require('koa');
+var router = require('koa-router')();
 var logger = require('koa-logger');
 var xmlParser = require('koa-xml-body').default; // note the default
 var mongo = require('koa-mongo');
 var app = koa();
+var Weixin = require('./weixin/weixin')('zhanfang');
 
-app.use(logger());
-
-app.use(xmlParser());
-app.use(mongo({
-  db:'lab',
-}));
-app.use(function*(next) {
+var handler = function*(next) {
   var data = this.request.body.xml;
   var msg = {};
   msg.ToUserName = data.ToUserName[0];
   msg.FromUserName = data.FromUserName[0];
   msg.CreateTime = data.CreateTime[0];
   msg.MsgType = data.MsgType[0];
-
   switch (msg.MsgType) {
     case 'text':
       msg.Content = data.Content[0];
@@ -42,16 +37,27 @@ app.use(function*(next) {
     default:
   }
   yield next;
-});
+}
 
-app.use(function*(){
-  this.body = this.mongo.db('lab').collection('users').findOne();
-});
+router.post('/', Weixin.handler());
+router.get('/home', Weixin.webHome());
+
+app.use(logger());
+
+app.use(mongo({
+  db: 'weixin',
+}));
+
+app.use(xmlParser());
+
+app.use(router.routes())
+  .use(router.allowedMethods());
+
 
 //app.use(function*() {
 //  this.body = '<xml><ToUserName><![CDATA[oB00duIh1UoR9KtBRiKfh-GMOmYQ]]></ToUserName><FromUserName><![CDATA[gh_28617bcf9338]]></FromUserName><CreateTime>1459230117.465</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[这是文本回复]]></Content></xml>';
 //})
-app.on('error',function(err){
+app.on('error', function(err) {
   console.log(err);
 })
 app.listen(3333);
---
+++ weixin/weixin.js
@@ -0,0 +1,91 @@
+var render = require('../lib/render');
+var to = require('../lib/to');
+
+var Weixin = function(token) {
+  if (!(this instanceof Weixin)) {
+    return new Weixin(token);
+  }
+  this.token = token;
+  this.res = null;
+}
+
+// Weixin.prototype.handlerText = function(msg) {
+//   return new Promise(function(resolve, reject) {});
+// }
+
+//将 js 组装成 xml
+Weixin.prototype.toXML = function(data) {
+  return to.toXml(data);
+};
+
+/*
+* 1.存储到数据库
+* 2.查询数据库
+* 3.返回
+*/
+Weixin.prototype.handler = function() {
+  //此处this==Weixin
+  var self = this;
+  return function*(next) {
+    //此处this=app.context
+    var data = this.request.body.xml;
+    var msg = {};
+    msg.ToUserName = data.ToUserName[0];
+    msg.FromUserName = data.FromUserName[0];
+    msg.CreateTime = data.CreateTime[0];
+    msg.MsgType = data.MsgType[0];
+    switch (msg.MsgType) {
+      case 'text':
+        msg.Content = data.Content[0];
+        msg.MsgId = data.MsgId[0];
+        yield this.mongo.db('weixin').collection('logtext').insert(msg);
+        var val = yield this.mongo.db('weixin').collection('keywords').findOne({
+          'key': msg.Content
+        }, {
+          _id: 0,
+          key: 0
+        });
+        var reMsg = {
+          FromUserName: msg.ToUserName,
+          ToUserName: msg.FromUserName,
+          Content: val.val,
+          CreateTime: Date.now() / 1000
+        };
+        yield this.mongo.db('weixin').collection('logtext').insert(reMsg);
+        var data = to.toXml(reMsg);
+        console.log(data);
+        this.body = data;
+        // yield self.handlerText(msg);
+        break;
+      case 'image':
+        msg.PicUrl = data.PicUrl[0];
+        msg.MsgId = data.MsgId[0];
+        msg.MediaId = data.MediaId[0];
+        console.log(msg);
+        break;
+      case 'voice':
+        msg.MediaId = data.MediaId[0];
+        msg.Format = data.Format[0];
+        msg.MsgId = data.MsgId[0];
+        console.log(msg);
+        break;
+      case 'event':
+        msg.Event = data.Event[0];
+        msg.EventKey = data.EventKey[0];
+        console.log(msg);
+        break;
+      default:
+    }
+    yield next;
+  }
+};
+
+Weixin.prototype.webHome = function() {
+  return function*() {
+    var user = yield this.mongo.db('weixin').collection('users').findOne();
+    console.log(user);
+    this.body = yield render('index');
+  }
+};
+
+module.exports = Weixin;
